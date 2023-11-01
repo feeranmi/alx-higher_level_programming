9d86bddc80d3e11eee9e0ffdf123aa2c46462591
feeranmi <verabiz554@gmail.com>
2023-11-01 13:20:02 +0100

    copy shallow list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    copy_list = my_list[:]
     if idx >= len(my_list) or idx < 0:
-        return (my_list.copy())
-    my_list[idx] = element
-    return my_list
+        return copy_list
+    copy_list[idx] = element
+    return copy_list
